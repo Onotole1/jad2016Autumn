3946d5f4baf20466df3d188b61d09cbba44abecb
Eugene Sorokin <jeksor.cover@gmail.com>
2016-09-24 17:34:41 +0700

    [feature] Add ui to main activity.

--- app/src/main/java/com/sample/jadcourse16autumn/ui/MainActivity.java
+++ app/src/main/java/com/sample/jadcourse16autumn/ui/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.sample.jadcourse16autumn.R;
+
 /**
  * Date: 24.09.2016
  * Time: 15:14
@@ -14,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 	}
 }
