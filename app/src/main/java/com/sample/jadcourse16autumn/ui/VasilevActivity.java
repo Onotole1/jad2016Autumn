ee500c5edf3f606f03434ae743a27c13fe7aba4f
Aleksei Vasilev <aleksei@vasiliev.biz>
2016-09-24 14:22:31 +0300

    [feature] Add my own activity.

---
+++ app/src/main/java/com/sample/jadcourse16autumn/ui/VasilevActivity.java
@@ -0,0 +1,15 @@
+package com.sample.jadcourse16autumn.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sample.jadcourse16autumn.R;
+
+public class VasilevActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vasilev);
+    }
+}
