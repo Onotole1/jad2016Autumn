a96bce6dacb7cd1cd0aa06165412a50d26c81bea
Eugene Sorokin <jeksor.cover@gmail.com>
2016-09-24 16:25:08 +0700

    Init commit.

---
+++ app/src/main/java/com/sample/jadcourse16autumn/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sample.jadcourse16autumn;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Date: 24.09.2016
+ * Time: 15:14
+ *
+ * @author Jeksor
+ */
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
